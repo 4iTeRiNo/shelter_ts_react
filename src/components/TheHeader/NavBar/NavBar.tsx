488d12fc0d33c8d04a6d2cf292a90dcdc9a9b699
4iTeRiNo(Igor) <zubkovigor@mail.ru>
2023-08-26 12:24:23 +0200

    add burger menu

--- src/components/TheHeader/NavBar/NavBar.tsx
+++ src/components/TheHeader/NavBar/NavBar.tsx
@@ -1,23 +1,55 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./NavBar.module.scss";
 import { NavLinks } from "./NavLinks";
-
-// export const NavBar = () => {
-//   return (
-//     <>
-//       <NavLinks />
-//     </>
-//   );
-// };
+import { FiMenu } from 'react-icons/fi';
+import cn from "classnames";
+import { link } from "fs";
 
 
 export const NavBar = () => {
+
+  const [navbarOpen, setNavBarOpen] = useState(false)
+  const btnClass = cn({
+    [styles.container]: true,
+    [styles.showMenu]: navbarOpen,
+  })
+
+  // const ref = useRef()
+  // useEffect(() => {
+  //   const handler = (event) => {
+  //     if (navbarOpen &&
+  //       ref.current &&
+  //       !ref.current.contains(event.target)
+  //     ) {
+  //       setNavBarOpen(false)
+  //     }
+  //   };
+  //   document.addEventListener('mousedown', handler)
+  //   return () => {
+  //     document.removeEventListener('mousedown', handler)
+  //   }
+  // }, [navbarOpen])
+
   return (
-    <nav className={styles.navBar}>
-      <ul className={styles.container}>
-        <NavLinks to='/' children="About the shelter"/>
-        <NavLinks to='/about' children="Our pets"/>
-        <NavLinks to='/' children="Help the shelter"/>
-        <NavLinks to='/footer' children="Contact"/>
+    <nav className={styles.navBar} >
+      <button
+        className={styles.toggle}
+        onClick={() => setNavBarOpen((prev) => !prev)}>
+        {navbarOpen ? (<FiMenu style={{
+          width: '40px', height: '35px', color: ' #f1cdb3',
+          strokeWidth: '1.5',
+          transform: `rotate(${90}deg)`
+        }} />
+        ) :
+          (<FiMenu style={{ width: '40px', height: '35px', color: ' #f1cdb3', strokeWidth: '1.5', }} />
+          )
+        }
+      </button>
+      <ul className={btnClass}>
+        <NavLinks to="./" onClick={() => setNavBarOpen(false)} children="About the shelter" />
+        <NavLinks to='/#about' onClick={() => setNavBarOpen(false)} children="Our pets" />
+        <NavLinks to='/#link' onClick={() => setNavBarOpen(false)} children="Help the shelter" />
+        <NavLinks to='/hai' onClick={() => setNavBarOpen(false)} children="Contact" />
       </ul>
     </nav>
   );
--- src/components/TheHeader/NavBar/NavLinks/NavLinks.tsx
+++ src/components/TheHeader/NavBar/NavLinks/NavLinks.tsx
@@ -2,13 +2,16 @@ import styles from "./NavLinks.module.scss";
 import { NavLink } from "react-router-dom";
 interface NavLinksProps {
   to: string;
-  children: string;
+  children: React.ReactNode;
+  onClick: () => void;
 }
 
 export const NavLinks = ({ children, to }: NavLinksProps) => {
   return (
     <li className={styles.item}>
-      <NavLink  to={to} className={(isActive) => styles.link + (isActive ? "" : 'active')}>
+      <NavLink to={to}
+        onClick={() => Boolean}
+        className={(isActive) => styles.link + (isActive ? "" : 'active')}>
         {children}
       </NavLink>
     </li>
